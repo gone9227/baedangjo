aa41860fa269f7c39fddb919dd779dae60a22f4c
gone9227 <gone9227@gmail.com>
2021-02-26 16:54:26 +0900

    first

---
+++ src/main/java/com/test/config/demo/DemoController.java
@@ -0,0 +1,14 @@
+package com.test.config.demo;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class DemoController {
+
+    @GetMapping("/hello")
+    public String hello(){
+        return "hello";
+    }
+
+}
